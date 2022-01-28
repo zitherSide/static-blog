865b3c2db472b3f2700147735d89b23b5acd901d
zitherSide <zitherSide@users.noreply.github.com>
2022-01-28 00:55:03 +0000

    deploy: cbac78d9b4854060aa9de3a4c7b81b6b8500c434

--- docs/_nuxt/static/1643331297/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fstatic-blog\u002F_nuxt\u002Fstatic\u002F1643331297",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fstatic-blog\u002F",assetsPath:"\u002Fstatic-blog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
